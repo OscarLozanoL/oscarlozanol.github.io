02be0a0dc5e459d847526fef8ff9dd730efd54ea
OscarLozanoL <oscar.felipe.lozano.lopez@gmail.com>
2024-04-09 20:39:57 -0600

    read json

--- JS/museum.js
+++ JS/museum.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
-    $('#generatedMuseum').DataTable();
 
-    $("#generar").onClic(function () {
-        alert("generar");
+    $("#generar").on("click", function () {
+        alert("Handler for `click` called.");
     });
+    $.getJSON("https://bioinfobot.github.io/data/2017-05.json")
+        .done(function (data) {
+            console.log(data)
+        });
 
 });
 
