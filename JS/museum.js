517439046906b818520b0024780161242e0f4f8d
OscarLozanoL <oscar.felipe.lozano.lopez@gmail.com>
2024-04-29 19:55:24 -0600

    button

--- JS/museum.js
+++ JS/museum.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
 
     $("#generar").on("click", function () {
         alert("Handler for `click` called.");
+        var newjson = $.getJSON("https://oscarlozanol.github.io/resources/museums.json")
+            .done(function (newjson) {
+                console.log(newjson)
+            });
     });
-    var newjson = $.getJSON("https://oscarlozanol.github.io/resources/museums.json")
-        .done(function (data) {
-            console.log(data)
-        };
+    
 
 });
 
